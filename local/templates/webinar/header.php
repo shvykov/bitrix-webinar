7a009abee7dcbd5f9b57386c16ba5a8a761ada60
Roman Shvykov <roman_shvykov@mail.ru>
2018-08-06 17:06:52 +0300

    Bitrix вебинар: пустой шаблон сайта

---
+++ local/templates/webinar/header.php
@@ -0,0 +1,12 @@
+<?php
+/** @var $APPLICATION CMain */
+/** @var $USER CUser */
+global $APPLICATION, $USER;
+?><!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <title><? $APPLICATION->ShowTitle() ?></title>
+    <? $APPLICATION->ShowHead() ?>
+</head>
+<body>
